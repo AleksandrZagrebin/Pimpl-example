86e39a6cfcf698fe859b6e9524375de6d13072d9
Aleksandr Zagrebin <132539805+AleksandrZagrebin@users.noreply.github.com>
2026-07-12 16:33:27 +0400

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -9,22 +9,27 @@ User::~User() = default;
 User::User(User&&) noexcept = default;
 User& User::operator=(User&&) noexcept = default;
 
-std::string User::getName() const {
+std::string User::getName() const
+{
     return pImpl->name;
 }
 
-int User::getAge() const {
+int User::getAge() const
+{
     return pImpl->age;
 }
 
-void User::setName(const std::string& name) {
+void User::setName(const std::string& name)
+{
     pImpl->name = name;
 }
 
-void User::setAge(int age) {
+void User::setAge(int age)
+{
     pImpl->age = age;
 }
 
-void User::print() const {
+void User::print() const 
+{
     std::cout << "User: " << pImpl->name << ", " << pImpl->age << " years old\n";
 }
