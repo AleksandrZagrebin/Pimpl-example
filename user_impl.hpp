c6f67cf6e354887f0b3192274da753e839fc081b
Aleksandr Zagrebin <132539805+AleksandrZagrebin@users.noreply.github.com>
2026-07-12 16:29:48 +0400

    Create user_impl.hpp

---
+++ user_impl.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "user.hpp"
+#include <iostream>
+
+struct User::Impl {
+    std::string name;
+    int age;
+
+    Impl(const std::string& n, int a) : name(n), age(a) {}
+};
